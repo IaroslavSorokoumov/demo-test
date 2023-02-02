9dc6c72613723d6386e57714146f01e1381f9ced
Iaroslav Sorokoumov <sorokoumov2@gmail.com>
2023-02-02 22:26:21 +0300

    commit 14-2

--- skillbox_14-2.py
+++ skillbox_14-2.py
@@ -9,20 +9,20 @@
 #
 # gcd(4782, 698)
 
-x1 = float(input('Введите X1: '))
-y1 = float(input('Введите Y1: '))
+# x1 = float(input('Введите X1: '))
+# y1 = float(input('Введите Y1: '))
 
-x2 = float(input('Укажите X2: '))
-y2 = float(input('Укажтите Y2: '))
+# x2 = float(input('Укажите X2: '))
+# y2 = float(input('Укажтите Y2: '))
 
-x_diff = x1 - x2
-y_diff = y1 - y2
-print("Уравнение прямой, проходящей через эти точки:")
-if x_diff == 0:
-    print("x =", x1)
-elif y_diff == 0:
-    print('y =', y1)
-else:
-    k = y_diff / x_diff
-    b = y2 - k * x2
-    print("y = ", k, " * x + ", b)
+# x_diff = x1 - x2
+# y_diff = y1 - y2
+# print("Уравнение прямой, проходящей через эти точки:")
+# if x_diff == 0:
+#     print("x =", x1)
+# elif y_diff == 0:
+#     print('y =', y1)
+# else:
+#     k = y_diff / x_diff
+#     b = y2 - k * x2
+#     print("y = ", k, " * x + ", b)
